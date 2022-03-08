ac710caab301b9f199427dcf9d8f23db7214e3ef
Simone Montanari <simone.montanari14@studio.unibo.it>
2022-03-08 14:32:09 +0100

    Es4 and test for Es4 lab02

---
+++ src/u02/Es4.scala
@@ -0,0 +1,15 @@
+package u02
+
+object Es4 extends App{
+
+  val p1 : Int => Int => Int => Boolean = x => y => z => x <= y && y <= z
+
+  val p2: (Int, Int, Int) => Boolean = (x, y, z) => x <= y && y <= z
+
+  def p3(x: Int)(y: Int)(z: Int): Boolean =
+    x <= y && y <= z
+
+  def p4(x: Int, y: Int, z: Int): Boolean =
+    x <= y && y <= z
+
+}
---
+++ test/u02/TestEs4.scala
@@ -0,0 +1,28 @@
+package u02
+
+import org.junit.jupiter.api.Assertions.{assertEquals, assertFalse, assertTrue}
+import org.junit.jupiter.api.Test
+import Es4.*
+
+class TestEs4:
+
+  @Test
+  def testp1(): Unit =
+    assertTrue(p1(2)(3)(4))
+    assertFalse(p1(2)(3)(2))
+
+  @Test
+  def testp2(): Unit =
+    assertTrue(p2(3,4,7))
+    assertFalse(p2(5,4,4))
+
+  @Test
+  def testp3(): Unit =
+    assertTrue(p3(2)(3)(5))
+    assertFalse(p3(2)(3)(1))
+
+  @Test
+  def testp4(): Unit =
+    assertTrue(p4(3,4,4))
+    assertFalse(p4(5,4,4))
+
